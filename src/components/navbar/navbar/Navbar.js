23f876ea20bf8d902f578ab89a6944259ae8fc78
Ugur Soylu <usoylu@mysoly.com>
2022-08-06 17:17:53 -0400

    Navbar Comp. is done!

---
+++ src/components/navbar/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <a href="#aboutUs">ABOUT US</a>
+        <a href="forYou">FOR YOU</a>
+        <a href="service">SERVICES</a>
+        <a href="blog">BLOG</a>
+        <a href="vlog">VLOG</a>
+        <a href="contact">CONTACT</a>
+    </div>
+  )
+}
+
+export default Navbar
